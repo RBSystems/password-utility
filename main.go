ae99672c16bd1ccac6507ecec1e809fd57f84aa5
Clark Green <clark_green@byu.edu>
2018-01-23 10:42:00 -0700

    hello from cMonster

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/byuoitav/raspi-deployment-microservice/passwords"
+	"github.com/fatih/color"
+)
+
+func main() {
+
+	//fetches the arguments without the path to the executable
+	args := os.Args[1:]
+
+	result, err := passwords.GetPassword(args[0])
+	if err != nil {
+		msg := fmt.Sprintf("passord not found: %s", err.Error())
+		log.Printf("%s", color.HiRedString("[password-utility] %s", msg))
+	}
+
+	fmt.Printf(result)
+
+}
